e651ab10ba6da305e470feb75e875ebb0d0fc016
ibidunife <53936340+ibidunife@users.noreply.github.com>
2020-01-29 11:30:31 +0100

    Add files via upload

---
+++ scriptt.js
@@ -0,0 +1,10 @@
+const player = 'bobby';
+let experience = 100;
+let wizardLevel = false;
+
+if (experience > 90) {
+    let wizardLevel =true;
+    console.log('inside', wizardLevel);
+}
+
+console.log('outside', wizardLevel);
